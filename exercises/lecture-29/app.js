fd49ae3a7afb5b45774d07e0414e5aba0a829585
Damien <damienambient@gmail.com>
2024-04-08 23:40:33 +0300

    lecture-29

---
+++ exercises/lecture-29/app.js
@@ -0,0 +1,50 @@
+class AuthException extends Error {
+    constructor(code, message) {
+        super(message ? `${code}: ${message}` : code);
+    }
+
+    toString() {
+        return this.message;
+    }
+}
+
+let isAuth = (auth) => auth ?? false;
+// let isAuth = (auth) => auth ?? true;
+let dialogBoxId = document.getElementById("dialogBox");
+
+const checkAuth = document.querySelector(".check-auth");
+const errorMessage = document.createElement("p");
+
+checkAuth.addEventListener("click", () => {
+    try {
+        if (!isAuth()) {
+            throw new AuthException(
+                "FORBIDDEN",
+                "You don't have access to this page"
+            );
+        } else {
+            window.open("success.html", "_blank");
+        }
+    } catch (e) {
+        errorMessage.classList.add("message");
+        errorMessage.textContent = e.toString();
+        dialogBoxId.appendChild(errorMessage);
+        showDialog();
+    }
+});
+
+function showDialog(e) {
+    dialogBoxId.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+            e.preventDefault();
+            closeDialog();
+        }
+    });
+
+    dialogBoxId.showModal();
+}
+
+function closeDialog() {
+    dialogBoxId.removeChild(errorMessage);
+    dialogBoxId.close();
+}
