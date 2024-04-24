703ea117dc175e726ea665cdeda4e2627ac92b16
Damien <damienambient@gmail.com>
2024-04-24 23:00:23 +0300

    lecture-34

--- react/blog/src/App.jsx
+++ react/blog/src/App.jsx
@@ -1,26 +1,15 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
 
 function App() {
-    const jsxElement = <h1>I am a JSX element</h1>;
-    const header = (
-        <header>
-            <h1>Welcome to React</h1>
-            <h2>Getting Started React</h2>
-            <h3>JavaScript Library</h3>
-        </header>
-    );
-    const footer = <p>Copyright &copy; 2024</p>;
     return (
-        <div>
-            {jsxElement}
-            {header}
-            <>
-                <h1>Welcome to React</h1>
-                <h2>Getting Started React</h2>
-                <h3>JavaScript Library</h3>
-            </>
-            {footer}
-        </div>
+        <main>
+            <Navbar></Navbar>
+            <Gallery></Gallery>
+            <Footer></Footer>
+        </main>
     );
 }
 
---
+++ react/blog/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 MyWebsite. All rights reserved.</p>
+        </footer>
+    );
+}
